402d6f17f215ab6f420c450e4f82c84996c3162b
Newot <wouter@xtal-protocols.de>
2024-07-30 10:29:01 +0200

    changed makeWordList to prevent an issue, where for some character lengths the Code would add extra characters to the output file. Also made the code more readable by using fscanf to check for spaces

--- makeWordList.c
+++ makeWordList.c
@@ -6,9 +6,9 @@
  static int commentLength = 43;
 
 //Replaces chars specified in specialChars with whatever is in replacementChars. Only works for 2-wide multi-characters and 2 characters
- void replaceAllSpecialChars(char* singleWord, int specialChars[], char *replacementChars[], int numberOfChars){
+ void _replaceAllSpecialChars(char* singleWord, int specialChars[], char *replacementChars[], int numberOfReplacementChars){
     char* result;
-    for(int i=0;i<numberOfChars;i++){
+    for(int i=0;i<numberOfReplacementChars;i++){
         do{
             result = strchr(singleWord, specialChars[i]);
             if(result != NULL){
@@ -20,7 +20,7 @@
 }
 
 //Copies the first commentLength chars over to the new file. Intended for legal information etc.
-void copyComments(FILE *output, FILE *wordDatabase, int commentLength){
+void _copyComments(FILE *output, FILE *wordDatabase, int commentLength){
     for(int i=0;i<commentLength;i++){
         char singleWord[128] = "";
         fgets(singleWord, 128, wordDatabase);
@@ -28,54 +28,43 @@ void copyComments(FILE *output, FILE *wordDatabase, int commentLength){
     }
 }
 
-int main(int argc, char *argv[]) {
-    printf("Number of Characters:");
-    int inputnumber = 0;
-    scanf("%i", &inputnumber);
-    printf("Result is in output.txt\n");
-
+void makeWordList(int numberOfCharacters){
     FILE *wordDatabase;
     FILE *output;
 
     output = fopen("output.txt", "w");
     wordDatabase = fopen("wordDatabase.txt", "r");
 
-    copyComments(output, wordDatabase, commentLength);
+    _copyComments(output, wordDatabase, commentLength);
 
     char singleWord[128];
-    while(fgets(singleWord, 128, wordDatabase)) {
-        bool isInputNumberLong = true;
-        int i = inputnumber;
-        //We figure out if the word has the correct length by looking for word separetor (' ')
-        if(singleWord[i]!=' '){
-            isInputNumberLong = false;
-        }
-        i--;
-        while(i>0 && isInputNumberLong){
-            if(singleWord[i]==' '){
-                isInputNumberLong = false;
-            }
-            i--;
-        }
-        if(isInputNumberLong){
+    while (fscanf(wordDatabase, "%s", singleWord)!=EOF) {
+        if(strlen(singleWord) == numberOfCharacters){
             singleWord[0] = tolower(singleWord[0]);
-            int numberOfChars = 7;
+            int numberOfReplacementChars = 7;
             //replacing the following chars. The 2 rows bellow are equivalent for UTF-8
             //int specialChars[] = {'ß', 'ö', 'ü', 'ä', 'Ö', 'Ü', 'Ä'};
             int specialChars[] = {-97, -74, -68, -92, -106, -100, -124};
             char *replacementChars[] = {"ss", "oe", "ue","ae", "oe", "ue","ae"};
-            replaceAllSpecialChars(singleWord, specialChars, replacementChars, numberOfChars);
-            
-            //only copying the word to the new file, not extra information
-            char firstNLettersFromString[inputnumber+1];
-            for(int i=0; i<inputnumber; i++){
-                firstNLettersFromString[i] = singleWord[i];
-            }
-            firstNLettersFromString[inputnumber] = '\n';
-            fprintf(output, firstNLettersFromString);
-            
+            _replaceAllSpecialChars(singleWord, specialChars, replacementChars, numberOfReplacementChars);
+            //writing word to file
+            fprintf(output, singleWord);
+            fprintf(output, "\n");
         }
+        //skipping all of the unneeded data after the word
+        char throwaway[128];
+        fgets(throwaway, 128, wordDatabase);
     }
+
     fclose(output);
     fclose(wordDatabase); 
+}
+
+int main(int argc, char *argv[]) {
+    printf("Number of Characters:");
+    int inputNumber = 0;
+    scanf("%i", &inputNumber);
+    printf("Result is in output.txt\n");
+
+    makeWordList(inputNumber);
 }
